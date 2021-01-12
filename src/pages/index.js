4d72892fa0c1abaca1b943ca34936536a95b9b33
DaqoLee <2662114014@qq.com>
2021-01-12 22:05:31 +0800

    up

--- src/pages/index.js
+++ src/pages/index.js
@@ -57,7 +57,7 @@ function Home() {
   const {siteConfig = {}} = context;
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
       <header className={clsx('hero hero--primary', styles.heroBanner)}>
         <div className="container">
@@ -70,7 +70,7 @@ function Home() {
                 styles.getStarted,
               )}
               to={useBaseUrl('docs/')}>
-              Get Started
+              Portal
             </Link>
           </div>
         </div>
