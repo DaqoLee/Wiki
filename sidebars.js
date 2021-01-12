ab64ba10888ba04dceef669dcacbba5cdd2393e7
DaqoLee <2662114014@qq.com>
2021-01-12 17:55:11 +0800

    测试文章

--- sidebars.js
+++ sidebars.js
@@ -1,7 +1,29 @@
 module.exports = {
-  someSidebar: {
-    Start:[
-      'template',
-    ]
-  },
+
+	readme:[
+		'template',
+		{
+			type: "category",
+			label: "搭建知识库",
+			collapsed: true,
+			items: [
+				'test',
+			],
+		}
+	]
+
+  // someSidebar: {
+  //   readme:[
+	// 		'template',
+	// 		{
+	// 			type: "category",
+	// 			label: "搭建知识库",
+	// 			collapsed: true,
+	// 			items: [
+	// 				'test',
+	// 			],
+	// 		}
+	// 	]
+		
+  // },
 };
